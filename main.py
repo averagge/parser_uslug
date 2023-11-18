eab5e4a391f9e099c62c29b3b4360b49d52b5e9c
averagge <aleksejporfirev15@gmail.com>
2023-11-18 18:15:24 +0300

    Добавил функцию для парсинга информации по услугам, услуги сохраняются в файле json

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 import os
+
+def takeservice(category3):
+    category3_link = category3.get("href")
+    category3_name = category3.text.replace("/", "_")
+    page = ""
+    page_num = 1
+    service = []
+    while page_num < 8:
+        try:
+            req = requests.get(f"https://uslugio.com{category3_link}{page}", headers=headers)
+            soup = BeautifulSoup(req.content, "lxml")
+            uslugi = soup.find_all("div", class_="item_data")
+            for usluga in uslugi:
+                title = usluga.find("div", class_="title showone").find("b").text
+                adress = usluga.find("span", class_="addres").text
+                description = usluga.find("div", class_="truncate-text").text.replace("\r", "")
+                description = description.replace("\n", "")
+                phone = usluga.find("strong", title="Номер телефона исполнителя").text
+                try:
+                    name = usluga.find("span", title="Имя исполнителя").text
+                except Exception:
+                    name = "No name"
+                service.append(
+                    {"title": title, "adress": adress, "description": description, "phone": phone,
+                     "name": name})
+            page_num += 1
+            page = f"?page={page_num}"
+        except Exception:
+            break
+    a = [category3_name, service]
+    return a
 
 url = "https://uslugio.com/"
 
@@ -34,22 +66,28 @@ for city in city_list:
                 try:
                     categories3 = soup.find("div", class_="row nav_theme").find_all("a")
                     for category3 in categories3:
-                        category3_link = category3.get("href")
-                        category3_name = category3.text.replace("/", "_")
-                        os.mkdir(f"data/{city_name}/{category1_name}/{category2_name}/{category3_name}")
-                        print(f"Создана папка: data/{city_name}/{category1_name}/{category2_name}/{category3_name}")
+                        category3_name = takeservice(category3)[0]
+                        service = takeservice(category3)[1]
+                        with open(f"data/{city_name}/{category1_name}/{category2_name}/{category3_name}.json", "w", encoding="utf-8") as file:
+                            json.dump(service, file, indent=4, ensure_ascii=False)
+                            print(f"Создан файл: data/{city_name}/{category1_name}/{category2_name}/{category3_name}.json")
+
                 except Exception:
-                    continue
+                    categories3 = soup.find("div", class_="row nav_cat_list").find_all("a")
+                    for category3 in categories3:
+                        category3_name = takeservice(category3)[0]
+                        service = takeservice(category3)[1]
+                        with open(f"data/{city_name}/{category1_name}/{category3_name}.json", "w", encoding="utf-8") as file:
+                            json.dump(service, file, indent=4, ensure_ascii=False)
+                            print(f"Создан файл: data/{city_name}/{category1_name}/{category3_name}.json")
         except Exception:
             req = requests.get(f"https://uslugio.com{category1_link}", headers=headers)
             soup = BeautifulSoup(req.content, "lxml")
             categories3 = soup.find("div", class_="row nav_theme").find_all("a")
             for category3 in categories3:
-                category3_link = category3.get("href")
-                category3_name = category3.text.replace("/", "_")
-                os.mkdir(f"data/{city_name}/{category1_name}/{category3_name}")
-                print(f"Создана папка: data/{city_name}/{category1_name}/{category3_name}")
-
-
-
-
+                category3_name = takeservice(category3)[0]
+                service = takeservice(category3)[1]
+                with open(f"data/{city_name}/{category1_name}/{category3_name}.json", "w",
+                          encoding="utf-8") as file:
+                    json.dump(service, file, indent=4, ensure_ascii=False)
+                    print(f"Создан файл: data/{city_name}/{category1_name}/{category3_name}.json")
